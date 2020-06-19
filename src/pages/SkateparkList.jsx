4a446ebb84b0d017b497c03ca5bcb56c85c38db6
Daryll <dcapistrano.dev@gmail.com>
2020-06-18 22:56:04 -0700

    wired up api to table to show data from database

--- src/pages/SkateparkList.jsx
+++ src/pages/SkateparkList.jsx
@@ -1,11 +1,100 @@
 import React, { Component } from "react";
+import ReactTable from "react-table-6";
+import api from "../api";
 
+import styled from "styled-components";
+
+import "react-table-6/react-table.css";
+
+const Wrapper = styled.div`
+  padding: 0 40px 40px 40px;
+`;
 class SkateparkList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      skateparks: [],
+      columns: [],
+      isLoading: false,
+    };
+
+    this.componentDidMount = async () => {
+      this.setState({ isLoading: true });
+
+      await api.getAllSkateparks().then((skateparks) => {
+        this.setState({
+          skateparks: skateparks.data.data,
+          isLoading: false,
+        });
+      });
+    };
+  }
+
   render() {
+    const { skateparks, isLoading } = this.state;
+    console.log("TCL: SkateparkList -> render -> skateparks", skateparks);
+
+    const columns = [
+      {
+        Header: "ID",
+        accessor: "_id",
+        filterable: true,
+      },
+      {
+        Header: "Name",
+        accessor: "name",
+        filterable: true,
+      },
+      {
+        Header: "Terrain",
+        accessor: "terrain",
+        filterable: true,
+      },
+      {
+        Header: "Location",
+        accessor: "location",
+        filterable: true,
+      },
+      {
+        Header: "City",
+        accessor: "city",
+        filterable: true,
+      },
+      {
+        Header: "State",
+        accessor: "state",
+        filterable: true,
+      },
+      {
+        Header: "Country",
+        accessor: "country",
+        filterable: true,
+      },
+      {
+        Header: "Rating",
+        accessor: "rating",
+        filterable: true,
+      },
+    ];
+
+    let showTable = true;
+    if (!skateparks.length) {
+      showTable = false;
+    }
+
     return (
-      <div>
-        <p>In this page you'll see the list of skateparks</p>
-      </div>
+      <Wrapper>
+        {showTable && (
+          <ReactTable
+            data={skateparks}
+            columns={columns}
+            loading={isLoading}
+            defaultPageSize={10}
+            showPageSizeOptions={true}
+            minRows={0}
+          />
+        )}
+      </Wrapper>
     );
   }
 }
